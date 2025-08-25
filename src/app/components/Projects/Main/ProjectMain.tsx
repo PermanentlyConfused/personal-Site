1a5d2898ded0aaf3ce28586dd0814f1941887764
Art <emailmadebyarthr@gmail.com>
2025-08-25 12:27:38 -0400

    reworked Projects Page

--- src/app/components/Projects/Main/ProjectMain.tsx
+++ src/app/components/Projects/Main/ProjectMain.tsx
@@ -68,108 +68,115 @@ const ProjectMain: React.FC = () => {
                 duration: 0.2,
                 delay: hasAnimated ? 0 : 0.2,
               }}
-              className="flex flex-col flex-wrap items-center justify-center gap-y-10 rounded-lg px-10 py-10"
+              className="grid grid-cols-3 grid-rows-3 items-center justify-center gap-10 rounded-lg px-10 py-10"
             >
-              {boxes.map((_, idx) => (
-                <div
-                  key={idx}
-                  onClick={() => setExpanded(idx)}
-                  className="flex h-70 w-[920px] cursor-pointer flex-col text-center transition"
-                >
-                  {idx == 0 && (
-                    <motion.div
-                      whileHover={{
-                        boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
-                      }}
-                      transition={{
-                        duration: 0.5,
-                        delay: 0,
-                        ease: "easeInOut",
-                      }}
-                      className="group/project-0 flex h-full w-120 items-end justify-center bg-[url('@/assets/Projects/LoRaSmartFarm/thumbnail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
-                      style={{
-                        alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
-                      }}
-                    >
-                      <div className="img-tape img-tape--1 bottom-75 left-15" />
-                      <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-0:-translate-y-30 group-hover/project-0:text-white">
-                        LoRaWAN-based Smart Farm
-                      </span>
-                    </motion.div>
-                  )}
-                  {idx == 1 && (
-                    <motion.div
-                      whileHover={{
-                        boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
-                      }}
-                      transition={{
-                        duration: 0.5,
-                        delay: 0,
-                        ease: "easeInOut",
-                      }}
-                      style={{
-                        alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
-                      }}
-                      className="group/project-1 flex h-full w-120 items-end justify-center bg-[url('@/assets/Projects/FPGAHand/Xilinx.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-900"
-                    >
-                      <div className="img-tape img-tape--1 bottom-75 left-25" />
-                      <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-1:-translate-y-30 group-hover/project-1:text-white">
-                        AI Enabled FPGA-based Robotic Hand
-                      </span>
-                    </motion.div>
-                  )}
-                  {idx == 2 && (
-                    <motion.div
-                      whileHover={{
-                        boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
-                      }}
-                      transition={{
-                        duration: 0.5,
-                        delay: 0,
-                        ease: "easeInOut",
-                      }}
-                      style={{
-                        alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
-                      }}
-                      className="group/project-2 flex h-full w-120 items-end justify-center bg-[url('@/assets/Projects/Fingerprint/fingerprintThumbnail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
-                    >
-                      <div className="img-tape img-tape--1 bottom-75 left-40" />
-                      <span className="mb-2 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-2:-translate-y-25 group-hover/project-2:text-white">
-                        FPGA-based Children Fingerprint Sensor and
-                        Authentication system
-                      </span>
-                    </motion.div>
-                  )}
-                  {idx == 3 && (
-                    <motion.div
-                      whileHover={{
-                        boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
-                      }}
-                      transition={{
-                        duration: 0.5,
-                        delay: 0,
-                        ease: "easeInOut",
-                      }}
-                      style={{
-                        alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
-                      }}
-                      className="group/project-3 flex h-full w-120 items-end justify-center bg-[url('@/assets/Projects/Boids/BoidsThumbNail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
-                    >
-                      <div className="img-tape img-tape--1 bottom-75 left-30" />
-                      <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-3:-translate-y-25 group-hover/project-3:text-white">
-                        Boids Simulation on Procedural Generated World
-                      </span>
-                    </motion.div>
-                  )}
-                  {/* {idx == 3 && (
+              {boxes.map((_, idx) => {
+                let positionClasses = [];
+
+                if (idx === 0) positionClasses[0] = "col-start-1 row-start-1"; // box1
+                if (idx === 1) positionClasses[1] = "col-start-1 row-start-3"; // box2
+                if (idx === 2) positionClasses[2] = "col-start-2 row-start-2"; // box3
+                if (idx === 3) positionClasses[3] = "col-start-3 row-start-1"; // box4
+                if (idx === 4) positionClasses[4] = "col-start-3 row-start-3"; // box5
+                return (
+                  <div
+                    key={idx}
+                    onClick={() => setExpanded(idx)}
+                    className={`w- h-70 cursor-pointer gap-y-10 text-center transition ${positionClasses[idx]}`}
+                  >
+                    {idx == 0 && (
+                      <motion.div
+                        whileHover={{
+                          boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
+                        }}
+                        transition={{
+                          duration: 0.5,
+                          delay: 0,
+                          ease: "easeInOut",
+                        }}
+                        className="group/project-0 flex h-full w-full items-end justify-center bg-[url('@/assets/Projects/LoRaSmartFarm/thumbnail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
+                        style={{}}
+                      >
+                        <div className="img-tape img-tape--1 bottom-75 left-15" />
+                        <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-0:-translate-y-30 group-hover/project-0:text-white">
+                          LoRaWAN-based Smart Farm
+                        </span>
+                      </motion.div>
+                    )}
+                    {idx == 1 && (
+                      <motion.div
+                        whileHover={{
+                          boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
+                        }}
+                        transition={{
+                          duration: 0.5,
+                          delay: 0,
+                          ease: "easeInOut",
+                        }}
+                        style={{
+                          alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
+                        }}
+                        className="group/project-1 flex h-full w-full items-end justify-center bg-[url('@/assets/Projects/FPGAHand/Xilinx.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-900"
+                      >
+                        <div className="img-tape img-tape--1 bottom-75 left-25" />
+                        <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-1:-translate-y-30 group-hover/project-1:text-white">
+                          AI Enabled FPGA-based Robotic Hand
+                        </span>
+                      </motion.div>
+                    )}
+                    {idx == 2 && (
+                      <motion.div
+                        whileHover={{
+                          boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
+                        }}
+                        transition={{
+                          duration: 0.5,
+                          delay: 0,
+                          ease: "easeInOut",
+                        }}
+                        style={{
+                          alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
+                        }}
+                        className="group/project-2 flex h-full w-full items-end justify-center bg-[url('@/assets/Projects/Fingerprint/fingerprintThumbnail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
+                      >
+                        <div className="img-tape img-tape--1 bottom-75 left-40" />
+                        <span className="mb-2 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-2:-translate-y-25 group-hover/project-2:text-white">
+                          FPGA-based Children Fingerprint Sensor and
+                          Authentication system
+                        </span>
+                      </motion.div>
+                    )}
+                    {idx == 3 && (
+                      <motion.div
+                        whileHover={{
+                          boxShadow: "0px 0px 19px 2px rgba(0,0,0,0.2)",
+                        }}
+                        transition={{
+                          duration: 0.5,
+                          delay: 0,
+                          ease: "easeInOut",
+                        }}
+                        style={{
+                          alignSelf: idx % 2 === 1 ? "flex-start" : "flex-end",
+                        }}
+                        className="group/project-3 flex h-full w-full items-end justify-center bg-[url('@/assets/Projects/Boids/BoidsThumbNail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800"
+                      >
+                        <div className="img-tape img-tape--1 bottom-75 left-30" />
+                        <span className="mb-2 px-5 text-2xl font-semibold text-black transition-colors duration-300 group-hover/project-3:-translate-y-25 group-hover/project-3:text-white">
+                          Boids Simulation on Procedural Generated World
+                        </span>
+                      </motion.div>
+                    )}
+                    {/* {idx == 3 && (
                       <div className="group/project-1 flex h-full w-full items-end justify-center bg-[url('@/assets/Projects/NagUTron/ThumbNail.png')] bg-cover bg-center bg-blend-soft-light hover:bg-gray-800">
                         <span className="mb-7 font-semibold text-black transition-colors duration-300 group-hover/project-1:text-white">
                           Nag-U-Tron
                         </span>
                       </div>
                     )} */}
-                </div>
-              ))}
+                  </div>
+                );
+              })}
             </motion.div>
           )}
         </AnimatePresence>
