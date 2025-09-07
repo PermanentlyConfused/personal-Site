ec8cb59dbc188935ceb0eca069cc97bd2c95692f
Art <emailmadebyarthr@gmail.com>
2025-09-07 12:52:33 -0400

    Updated Resume; modified logos; added layout to add pages

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "../globals.css";
+
+const montserrat = Montserrat({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Arial", "sans-serif"],
+  variable: "--font-montserrat",
+});
+
+export const metadata: Metadata = {
+  title: "Art Nguyen | About",
+  description: "Hi I'm Art",
+};
+
+export default function BookmarkLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/assets/logo2.png" type="image/png" />
+      </head>
+      <body className={` ${montserrat.variable} scroll-smooth antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
