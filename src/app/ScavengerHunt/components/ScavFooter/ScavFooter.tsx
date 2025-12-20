96a48069893bf76c5d8ee53e29d83768e73b0f36
Art <emailmadebyarthr@gmail.com>
2025-12-20 18:41:34 -0500

    Modified CSS for scavenger hunt header and footer

--- src/app/ScavengerHunt/components/ScavFooter/ScavFooter.tsx
+++ src/app/ScavengerHunt/components/ScavFooter/ScavFooter.tsx
@@ -11,8 +11,8 @@ import "../css/glass_background.css";
 const ScavFooter: React.FC = () => {
   return (
     <footer className="border-t-1 border-black bg-[#1e262e] text-gray-900">
-      <div className="mx-auto flex flex-col divide-gray-600 px-2 py-4 md:px-40 md:py-8 lg:flex-row lg:justify-between">
-        <div className="lg: flex flex-row items-center gap-3 space-y-2 self-center py-2 text-center font-[family-name:var(--font-montserrat)] font-semibold text-black md:flex-col md:space-y-0 md:space-x-4 lg:flex">
+      <div className="mx-auto flex flex-col divide-gray-600 px-2 py-5 md:px-40 md:py-8 lg:flex-row lg:justify-between">
+        <div className="lg: flex flex-row items-center gap-3 self-center py-4 text-center font-[family-name:var(--font-montserrat)] font-semibold text-black md:flex-col md:space-y-0 md:space-x-4 lg:flex">
           <p className="text-sm text-[#cbe3f5]">
             Thank you to our Corporate Sponsor
           </p>
@@ -21,36 +21,35 @@ const ScavFooter: React.FC = () => {
             alt="Global"
             height={100}
             width={200}
-            className="-translate-y-1"
           ></Image>
         </div>
-        <div className="flex flex-col justify-center lg:pt-0">
-          <div className="flex justify-end space-x-4 px-2">
+        <div className="flex flex-col justify-center">
+          <div className="flex flex-row justify-evenly gap-2 sm:justify-center">
             <a
-              href="#"
+              href="https://www.instagram.com/sase_clarkson/"
               title="Instagram"
-              className="md8-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-8"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-10 md:w-10"
             >
               <FaInstagram color="#0f6cb6" />
             </a>
             <a
               href="https://www.linkedin.com/in/cusase/"
               title="LinkedIn"
-              className="md8-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-8"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-10 md:w-10"
             >
               <FaLinkedin color="#0f6cb6" />
             </a>
             <a
               href="https://discord.gg/8JVSNvP5zZ"
               title="Discord"
-              className="md8-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-8"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-10 md:w-10"
             >
               <FaDiscord color="#0f6cb6" />
             </a>
             <a
               href="mailto:sase@clarkson.edu"
               title="Gmail"
-              className="md8-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-8"
+              className="flex h-8 w-8 items-center justify-center rounded-full bg-[#cbe3f5] text-gray-50 md:h-10 md:w-10"
             >
               <IoIosMail color="#0f6cb6" />
             </a>
