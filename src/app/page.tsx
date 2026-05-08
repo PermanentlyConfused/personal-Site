8aae7aca5284d4a54e962d3e3edacc04a0f77897
Art <emailmadebyarthr@gmail.com>
2026-05-08 14:47:17 -0400

    Pushing new Bun branch to test with ThreeJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import LeftPanel from "@/app/components/LeftPanel/LeftPanel";
 import NavBar from "@/app/components/NavBar/NavBar";
 
 export default function Home() {
-  return (
-    <div className="homeBG h-full">
-      {/* <DoodleBackgrounds /> */}
-      <div id="home" className="font-sans lg:px-12 lg:py-10">
-        <div className="flex-col justify-center font-[family-name:var(--font-montserrat)] lg:flex lg:flex-row lg:gap-20">
-          <LeftPanel />
-          <NavBar />
-        </div>
-      </div>
+    return (
+        <div className="homeBG min-h-full min-w-full">
+            {/* <DoodleBackgrounds /> */}
+            <div id="home" className="font-sans lg:px-12 lg:py-10">
+                <div className="flex-col justify-center font-[family-name:var(--font-montserrat)] lg:flex lg:flex-row lg:gap-20">
+                    <LeftPanel />
+                    <NavBar />
+                </div>
+            </div>
 
-      {/* <Footer /> */}
-      {/* <MyBookMarks /> */}
-      {/* <div id="midRight" className="homeBG text-black"></div> */}
-    </div>
-  );
+            {/* <Footer />  */}
+            {/* <MyBookMarks />  */}
+            {/* <div id="midRight" className="homeBG text-black"></div>  */}
+        </div>
+    );
 }
